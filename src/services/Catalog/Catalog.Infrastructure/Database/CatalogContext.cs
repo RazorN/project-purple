4b839a96d7eb2f8b14cb71bc6ffde0f019fa1f1a
Ras96 <ras96pl@gmail.com>
2021-04-09 13:53:14 +0200

    Create infrastructure project

---
+++ src/services/Catalog/Catalog.Infrastructure/Database/CatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Infrastructure.Database
+{
+    public class CatalogContext : DbContext
+    {
+    }
+}
---
+++ src/services/Catalog/Catalog.Infrastructure/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            var config = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+
+            services.AddDbContext<CatalogContext>(options =>
+                options.UseNpgsql(config.GetConnectionString("CatalogContext")));
+
+            return services;
+        }
+    }
+}
